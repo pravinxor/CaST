f63d89334d8fca5cab81d6e5872c6ec5f8e7d268
Pravin Ramana <pravinramana25@protonmail.ch>
2023-12-26 12:02:57 -0600

    Create patch tokenizer

---
+++ model/tokenizer.py
@@ -0,0 +1,27 @@
+from tinygrad import nn
+from tinygrad.tensor import Tensor
+
+
+class ConvolutionalTokenizer:
+    def __init__(
+        self,
+        input_channels: int,
+        embed_dim: int,
+        kernel_size: (int, int),
+        stride: int,
+        padding: int,
+        pooling_kernel_size: (int, int),
+        pooling_stride: int,
+    ):
+        self.conv = nn.Conv2d(input_channels, embed_dim, kernel_size, stride, padding)
+        self.pooling_kernel_size, self.pooling_stride = (
+            pooling_kernel_size,
+            pooling_stride,
+        )
+
+    def __call__(self, x: Tensor) -> Tensor:
+        x = self.conv(x)
+        x = x.relu()
+        x = x.max_pool2d(self.pooling_kernel_size, self.pooling_stride)
+        x = x.flatten(-2)
+        return x
