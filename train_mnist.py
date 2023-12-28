9c7079ed779de5548d8e65af050c5852f8cdb264
Pravin Ramana <pravinramana25@protonmail.ch>
2023-12-27 20:07:15 -0600

    Update dataloader functions and remove dead code

--- train_mnist.py
+++ train_mnist.py
@@ -1,5 +1,4 @@
 from datasets import load_dataset
-import numpy as np
 from model.cast import CaSTModel
 from dataloader import DataLoader
 
@@ -10,6 +9,15 @@ from tinygrad.tensor import Tensor
 from tqdm import tqdm
 
 
+def mnist_collate_images(batch):
+    images = list(map(lambda p: p["image"], batch))
+    images = Tensor(images) / 255
+    images = images.reshape(-1, 1, 28, 28)
+
+    labels = list(map(lambda s: s["label"], batch))
+    labels = Tensor(labels)
+    return images, labels
+
 
 ds = load_dataset("fashion_mnist")
 train = ds["test"]
@@ -17,46 +25,45 @@ test = ds["test"]
 
 n_classes = 10
 n_channels = 1
-model = CaSTModel(n_classes, n_channels, embed_dim=128, kernel_size=3, kernel_stride=1, padding=1, pooling_kernel_size=2, pooling_stride=1, n_encoders=2, n_attn_heads=2)
+model = CaSTModel(
+    n_classes,
+    n_channels,
+    embed_dim=128,
+    kernel_size=3,
+    kernel_stride=1,
+    padding=1,
+    pooling_kernel_size=2,
+    pooling_stride=1,
+    n_encoders=2,
+    n_attn_heads=2,
+)
 
 params = get_parameters(model)
 optim = AdamW(params, lr=1e-3, b1=0.9, b2=0.999)
 
-def collate_images(batch):
-    x = np.array(batch["image"]) / 255
-    x = x.reshape(-1, 1, 28, 28)
-    y = batch["label"]
-    return x, y
-
-def chunks(lst, n):
-    for i in range(0, len(lst), n):
-        yield lst[i:i + n]
-
-
-opt = AdamW(get_parameters(model))
-
 batch_size = 64
 epochs = 300
 
+loader = DataLoader(train, collate_fn=mnist_collate_images)
 for epoch in range(epochs):
     total_correct = 0
     total_samples = 0
-    trainloop = tqdm(chunks(train, batch_size))
-    for batch in trainloop:
-        x, y = collate_images(batch)
-        x, y = Tensor(x), Tensor(y)
+    trainloop = tqdm(loader)
+    for images, labels in trainloop:
+        X, Y = images, labels
 
-        
-        opt.zero_grad()
-        z = model(x)
-        loss = z.sparse_categorical_crossentropy(y).mean()
+        optim.zero_grad()
+        y = model(X)
+        loss = y.sparse_categorical_crossentropy(Y).mean()
         loss.backward()
-        opt.step()
+        optim.step()
 
-        predicted_labels = z.argmax(axis=1).numpy()
-        for pred, gt in zip(predicted_labels, y.numpy()):
+        predicted_labels = y.argmax(axis=1).numpy()
+        for pred, gt in zip(predicted_labels, Y.numpy()):
             if pred == gt:
                 total_correct += 1
-        total_samples += y.shape[0]
-        
-        trainloop.set_description_str(f"Correct: {total_correct}/{total_samples} Epoch {epoch}, Loss: {loss.numpy()}")
+        total_samples += Y.shape[0]
+
+        trainloop.set_description_str(
+            f"Correct: {total_correct}/{total_samples} Epoch {epoch}, Loss: {loss.numpy()}"
+        )
